ba3ff8ef52b6e74be6451ad28c40f8ba9d88c1bb
Meystergod <malahov_gleb@mail.ru>
2022-11-02 08:51:42 +0500

    update middleware's handling error for tests

--- internal/apperror/middleware.go
+++ internal/apperror/middleware.go
@@ -21,28 +21,28 @@ func Middleware(h appHandler) http.HandlerFunc {
 				if errors.Is(err, ErrorNotFound) {
 					w.WriteHeader(http.StatusNotFound)
 					w.Write(ErrorNotFound.Marshal())
-					logger.Errorf("%s with status code: %d. error code: %s", ErrorNotFound.Error(), http.StatusNotFound, ErrorNotFound.ErrorCode)
+					logger.Errorf("%s with status code: %d. error: %s", ErrorNotFound.Error(), http.StatusNotFound, ErrorNotFound.ErrorCode)
 					return
 				} else if errors.Is(err, ErrorDecode) {
 					w.WriteHeader(http.StatusBadRequest)
 					w.Write(ErrorDecode.Marshal())
-					logger.Errorf("%s with status code: %d. error code: %s", ErrorDecode.Error(), http.StatusBadRequest, ErrorDecode.ErrorCode)
+					logger.Errorf("%s with status code: %d. error: %s", ErrorDecode.Error(), http.StatusBadRequest, ErrorDecode.ErrorCode)
 					return
 				} else if errors.Is(err, ErrorEncode) {
 					w.WriteHeader(http.StatusBadRequest)
 					w.Write(ErrorDecode.Marshal())
-					logger.Errorf("%s with status code: %d. error code: %s", ErrorEncode.Error(), http.StatusBadRequest, ErrorEncode.ErrorCode)
+					logger.Errorf("%s with status code: %d. error: %s", ErrorEncode.Error(), http.StatusBadRequest, ErrorEncode.ErrorCode)
 					return
 				} else if errors.Is(err, ErrorValidate) {
 					w.WriteHeader(http.StatusBadRequest)
 					w.Write(ErrorValidate.Marshal())
-					logger.Errorf("%s with status code: %d. error code: %s", ErrorValidate.Error(), http.StatusBadRequest, ErrorValidate.ErrorCode)
+					logger.Errorf("%s with status code: %d. error: %s", ErrorValidate.Error(), http.StatusBadRequest, ErrorValidate.ErrorCode)
 					return
 				}
 				err = err.(*AppError)
 				w.WriteHeader(http.StatusBadRequest)
 				w.Write(ErrorUnknown.Marshal())
-				logger.Errorf("%s with status code: %d. error code: %s", ErrorUnknown.Error(), http.StatusBadRequest, ErrorUnknown.ErrorCode)
+				logger.Errorf("%s with status code: %d. error: %s", ErrorUnknown.Error(), http.StatusBadRequest, ErrorUnknown.ErrorCode)
 				return
 			}
 			w.WriteHeader(http.StatusTeapot)
